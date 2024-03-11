7ab821234f35654dc704e5e9dfcbe2db66d04d10
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-03-11 13:10:01 -0500

    added comments and log sample

--- Device.go
+++ Device.go
@@ -31,8 +31,9 @@ type Device struct {
 type DeviceInfo struct {
 	Id    uuid.UUID
 	Cd    uuid.UUID
-	Md    string
-	Fn    string
+	Bs    net.HardwareAddr //? MAC Address (used for Airplay 2) "airplay_device_id"
+	Md    string           //? Device type / Manufacturer
+	Fn    string           //? Friendly device name
 	other map[string]string
 	// id=UUID cd=UUID rm= ve=05 md=Google Home ic=/setup/icon.png fn=Kitchen speaker ca=199172 st=0 bs=??? nf=1 rs=
 }
@@ -45,6 +46,34 @@ func (i *DeviceInfo) IsTv() (bool, string) {
 	return strings.Contains(strings.ToLower(i.Fn), "tv"), i.Md
 }
 
+func (i *DeviceInfo) AirplayDeviceId() (string, net.HardwareAddr) {
+	return "airplay_device_id", i.Bs
+}
+
+func (i *DeviceInfo) AirplayDeviceName() (string, string) {
+	return "name", i.Fn
+}
+
+//    0.000032000 "shairport.c:2401" daemon status is 0.
+// 0.000040538 "shairport.c:1567" PID file: "/var/run/shairport-sync/shairport-sync.pid".
+// 0.000088770 "shairport.c:2402" daemon pid file path is "/var/run/shairport-sync/shairport-sync.pid".
+
+//          0.000070769 "rtsp.c:374" Creating metadata queue "multicast".
+// 0.007861923 "mdns_avahi.c:220" avahi: service '9483C43DCE3B@GL-MT3000' group is not yet committed.
+// 0.004553154 "mdns_avahi.c:277" avahi: avahi_entry_group_commit 0
+// 0.000261769 "mdns_avahi.c:477" avahi_dacp_monitor_start Avahi DACP monitor successfully started
+// 0.000255539 "mdns_avahi.c:224" avahi: service '9483C43DCE3B@GL-MT3000' group is registering.
+// 0.883230385 "mdns_avahi.c:191" avahi: service '9483C43DCE3B@GL-MT3000' successfully added.
+//          0.000028000 "audio_alsa.c:2039" keep_dac_busy is now "no"
+// 0.000063615 "shairport.c:2409" run_this_before_play_begins action is "(null)".
+// 0.000079616 "shairport.c:2410" run_this_after_play_ends action is "(null)".
+// 0.000027538 "shairport.c:2411" wait-cmd status is 0.
+// 0.000056385 "shairport.c:2412" run_this_before_play_begins may return output is 0.
+// 0.000026846 "shairport.c:2413" run_this_if_an_unfixable_error_is_detected action is "(null)".
+// 0.000025846 "shairport.c:2415" run_this_before_entering_active_state action is  "(null)".
+// 0.000026000 "shairport.c:2417" run_this_after_exiting_active_state action is  "(null)".
+// 0.000025385 "shairport.c:2419" active_state_timeout is  10.000000 seconds.
+
 func FromServiceEntryInfo(info []string) *DeviceInfo {
 	var d DeviceInfo
 	d.other = make(map[string]string)
@@ -56,6 +85,7 @@ func FromServiceEntryInfo(info []string) *DeviceInfo {
 	d.Cd = uuid.MustParse(d.other["cd"])
 	d.Md = d.other["md"]
 	d.Fn = d.other["fn"]
+	d.Bs = net.HardwareAddr(d.other["bs"])
 	return &d
 }
 
